072917d1651da1148f84b376f2d5dcad93e2291b
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-12-06 20:10:47 -0300

    ajusta requisitos

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const btnAdd = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const olList = document.querySelector('#lista-tarefas');
+const btnApagaTudo = document.querySelector('#apaga-tudo');
+const btnRemFin = document.querySelector('#remover-finalizados');
+const btnSalvar = document.querySelector('#salvar-tarefas');
+// console.log(btnSalvar);
+
+const salvar = () => {
+  localStorage.setItem('items', olList.innerHTML);
+};
+
+const removerFinalizados = () => {
+  btnRemFin.addEventListener('click', () => {
+    const lis = olList.children;
+    for (let i = 0; i < lis.length; i += 1) {
+      if (lis[i].classList.contains('completed')) {
+        lis[i].remove();
+      }
+    }
+  });
+};
+
+const apagaTudo = () => {
+  olList.innerHTML = '';
+};
+
+const addCompleted = ({ target }) => {
+  target.classList.toggle('completed');
+};
 
 const selecItem = ({ target }) => {
   olList.childNodes.forEach((li) => {
@@ -12,12 +39,27 @@ const selecItem = ({ target }) => {
   });
 };
 
+const adicionarEventos = (li) => {
+  li.addEventListener('click', selecItem);
+  li.addEventListener('dblclick', addCompleted);
+};
+
+const recuperar = () => {
+  olList.innerHTML = localStorage.getItem('items');
+  olList.childNodes.forEach(adicionarEventos);
+};
+
 const addTarefas = () => {
   const li = document.createElement('li');
   li.innerText = input.value;
-  li.addEventListener('click', selecItem);
+  adicionarEventos(li);
   olList.appendChild(li);
   input.value = '';
+  removerFinalizados();
 };
 
 btnAdd.addEventListener('click', addTarefas);
+btnApagaTudo.addEventListener('click', apagaTudo);
+btnSalvar.addEventListener('click', salvar);
+
+window.onload = recuperar;
